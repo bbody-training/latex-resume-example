c1de41b7cac6e921d281951da276292809101218
Brendon Body <bbody.project@gmail.com>
2019-01-17 14:36:01 +1100

    Fix up directories and output

--- compile.sh
+++ compile.sh
@@ -1,24 +1,26 @@
 BRANCH_NAME=$(git symbolic-ref --short -q HEAD)
 OUTPUT_DIRECTORY="Output"
+mkdir -p $OUTPUT_DIRECTORY
 
 if [[ "$BRANCH_NAME" != "master" ]]; then
 	YEAR=`date +'%Y'`
 	MONTH=`date +'%B'`
 
 	# Create directories if they are not there
-	mkdir -p OUTPUT_DIRECTORY
-
 	OUTPUT_DIRECTORY="$OUTPUT_DIRECTORY/$YEAR"
-	mkdir -p OUTPUT_DIRECTORY
+	mkdir -p $OUTPUT_DIRECTORY
 
 	OUTPUT_DIRECTORY="$OUTPUT_DIRECTORY/$MONTH"
-	mkdir -p OUTPUT_DIRECTORY
+	mkdir -p $OUTPUT_DIRECTORY
 
 	OUTPUT_DIRECTORY="$OUTPUT_DIRECTORY/$BRANCH_NAME"
-	mkdir -p OUTPUT_DIRECTORY
-else
-	mkdir -p OUTPUT_DIRECTORY
+	mkdir -p $OUTPUT_DIRECTORY
 fi
 
-output=`pdflatex -halt-on-error -output-directory $OUTPUT_DIRECTORY resume.tex`
-echo "$output"
+OUTPUT=`pdflatex -halt-on-error -output-directory "$OUTPUT_DIRECTORY" resume.tex`
+
+if [[ $? == 0 ]]; then
+	echo "Successfully written to $OUTPUT_DIRECTORY/resume.pdf"	
+else
+	echo $OUTPUT
+fi
